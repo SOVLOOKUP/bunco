f9b95d9660f459a59de16761109dcc0635d68a19
Starry North <gonorth@qq.com>
2025-04-05 14:04:55 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -77,13 +77,14 @@ export const compile = async (params: Input) => {
 
   // 打包命令
   const cmds = tgs.map((v, index) => {
+    const isWin32Platform = (a: string) => (v.includes("windows") ? a : "");
+
     const cmd =
       `bun build` +
-      `${hideConsole ? " --windows-hide-console " : " "}` +
-      `${icon ? ` --windows-icon=${icon} ` : " "}` +
+      isWin32Platform(`${hideConsole ? " --windows-hide-console " : " "}`) +
+      isWin32Platform(`${icon ? ` --windows-icon=${icon} ` : " "}`) +
       `${bytecode ? ` --bytecode ` : " "}` +
       `--compile --minify --sourcemap --target=bun-${v} --outfile "${outs[index]}" ${input}`;
-
     return {
       outFile: outs[index],
       platform: v,
